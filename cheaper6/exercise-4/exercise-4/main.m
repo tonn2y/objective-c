cfb1d04e3d55d33b1a9378b36dea5b0ad16dee6e
moony <moony@moonydeMacBook-Pro.local>
2015-01-24 20:06:06 +0800

    the chearp 6 program and exercise

--- cheaper6/exercise-4/exercise-4/main.m
+++ cheaper6/exercise-4/exercise-4/main.m
@@ -8,15 +8,102 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Calcalutor : NSObject
+@interface Calculator : NSObject
 
+//累加器方法
+-(void) setAccumulator:(double) value;
+-(void) clear;
+-(double) accumulator;
 
+//算术方法
+-(void) add:(double) value;
+-(void) subtract:(double) value;
+-(void) multply:(double) value;
+-(void) divide:(double) value;
+
+@end
+
+@implementation Calculator
+{
+    double accumulator;
+}
+
+-(void) setAccumulator:(double)value
+{
+    accumulator = value;
+}
+
+-(void) clear
+{
+    accumulator = 0;
+}
+
+-(double) accumulator
+{
+    return accumulator;
+}
+
+-(void) add:(double)value
+{
+    accumulator += value;
+}
+
+-(void) subtract:(double)value
+{
+    accumulator -= value;
+}
+
+-(void) multply:(double)value
+{
+    accumulator *= value;
+}
+
+-(void) divide:(double)value
+{
+    if ( value != 0.0) {
+        accumulator /= value;
+    } else {
+        NSLog(@"Division by zero.");
+        accumulator = NAN;
+    }
+}
 
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        double value;
+        char operator;
+        Calculator *deskCalc = [[Calculator alloc]init];
         
+        NSLog(@"Begin Calculations");
+        do{
+            scanf("%lf %c",&value,&operator);
+            switch (operator) {
+                case 'S':
+                    [deskCalc setAccumulator:value];
+                    break;
+                case '+':
+                    [deskCalc add:value];
+                    break;
+                case '-':
+                    [deskCalc subtract:value];
+                    break;
+                case '*':
+                    [deskCalc multply:value];
+                    break;
+                case '/':
+                    [deskCalc divide:value];
+                    break;
+                case 'E':
+                    NSLog(@"End of program");
+                    break;
+                default:
+                    break;
+            }
+            NSLog(@"= %lf",[deskCalc accumulator]);
+            
+        }while (operator != 'E');
     }
     return 0;
 }
