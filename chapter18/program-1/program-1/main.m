0b5bf273e0f0137133d1b21788ef9eae5d01c72c
moony <121087772@qq.com>
2015-05-01 11:05:41 +0800

    add chapter18 program

---
+++ chapter18/program-1/program-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  program-1
+//
+//  Created by moony on 15/5/1.
+//  Copyright (c) 2015年 moony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
